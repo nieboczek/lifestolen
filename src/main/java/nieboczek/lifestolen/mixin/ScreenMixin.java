dbc1bb9ef5d561537ea8ea2f9dbc9c4c45a2b4e0
nieboczek <bartekkoraliki@gmail.com>
2026-06-28 12:44:47 +0200

    fix: fix screen mixin

--- src/main/java/nieboczek/lifestolen/mixin/ScreenMixin.java
+++ src/main/java/nieboczek/lifestolen/mixin/ScreenMixin.java
@@ -1,19 +1,29 @@
 package nieboczek.lifestolen.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.input.KeyEvent;
 import nieboczek.lifestolen.Lifestolen;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
+    @Shadow
+    @Final
+    protected Minecraft minecraft;
 
-    @Override
-    public boolean keyPressed(KeyEvent keyEvent) {
-        if (minecraft.options.keySocialInteractions.matches(keyEvent)) {
-            Lifestolen.Companion.setKillSwitch(!Lifestolen.Companion.getKillSwitch());
-            return true;
+    @SuppressWarnings("ConstantValue")
+    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+    public void keyPressed(KeyEvent keyEvent, CallbackInfoReturnable<Boolean> cir) {
+        if (((Object) this) instanceof TitleScreen && minecraft.options.keySocialInteractions.matches(keyEvent)) {
+            Lifestolen.toggleKillSwitch();
+            cir.setReturnValue(true);
         }
-        return super.keyPressed(keyEvent);
     }
 }
