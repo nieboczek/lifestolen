65f6dc8d7e51d90174bab5a19098802c676aff5a
nieboczek <bartekkoraliki@gmail.com>
2026-07-30 16:39:51 +0200

    refactor: deprecate TextScale from ClientConfig

--- src/main/kotlin/nieboczek/lifestolen/config/ClientConfig.kt
+++ src/main/kotlin/nieboczek/lifestolen/config/ClientConfig.kt
@@ -6,7 +6,6 @@ import nieboczek.lifestolen.serializer.base.*
 class ClientConfig {
     // Populate with default values
     var renderClientBrandText = false
-    var textScale = 1f
     var friends = mutableListOf<String>()
     var commandPrefix = "."
 
@@ -14,7 +13,7 @@ class ClientConfig {
         const val ID: String = "Client"
         val serializer = ClassSerializer { ClientConfig() }
             .field("RenderClientBrandText", BooleanSerializer(), { it.renderClientBrandText }, { c, v -> c.renderClientBrandText = v })
-            .field("TextScale", FloatSerializer(), { it.textScale }, { c, v -> c.textScale = v })
+            .field("TextScale", FloatSerializer(), { 0f }, { _, _ -> }) // kept for backwards compat TODO: remove at some point
             .field("Friends", ListSerializer(StringSerializer()), { it.friends }, { c, v -> c.friends = v })
             .field("CommandPrefix", StringSerializer(), { it.commandPrefix }, { c, v -> c.commandPrefix = v })
     }
